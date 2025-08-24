7b3049b9c07cf4a4ebbf57bdb81f44d4f26e0eeb
Genouka <142009020+Genouka@users.noreply.github.com>
2025-08-24 20:51:50 +0800

    Update NativeLibrary.cs

--- src/Magick.NET/Native/NativeLibrary.cs
+++ src/Magick.NET/Native/NativeLibrary.cs
@@ -16,18 +16,18 @@ internal static class NativeLibrary
     public string Arm64Name = Name + "-" + QuantumName + "-arm64.dll";
 
 #if Q8
-    private string Quantum = "Q8";
+    public string Quantum = "Q8";
 #elif Q16
-    private string Quantum = "Q16";
+    public string Quantum = "Q16";
 #elif Q16HDRI
-    private string Quantum = "Q16-HDRI";
+    public string Quantum = "Q16-HDRI";
 #else
 #error Not implemented!
 #endif
 
 #if OPENMP
-    private string OpenMP = "-OpenMP";
+    public string OpenMP = "-OpenMP";
 #else
-    private string OpenMP = "";
+    public string OpenMP = "";
 #endif
 }
