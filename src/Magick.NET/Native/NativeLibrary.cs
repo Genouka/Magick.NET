73f5f59c3ca3d5409d7c510cdf22c871683705c7
Genouka <142009020+Genouka@users.noreply.github.com>
2025-08-24 21:06:12 +0800

    Update NativeLibrary.cs

--- src/Magick.NET/Native/NativeLibrary.cs
+++ src/Magick.NET/Native/NativeLibrary.cs
@@ -5,29 +5,29 @@ namespace ImageMagick;
 
 internal static class NativeLibrary
 {
-    public static string Name = "Magick.Native";
+    public const string Name = "Magick.Native";
 
-    public static string QuantumName = Quantum + OpenMP;
+    public const string QuantumName = Quantum + OpenMP;
 
-    public static string X86Name = Name + "-" + QuantumName + "-x86.dll";
+    public const string X86Name = Name + "-" + QuantumName + "-x86.dll";
 
-    public static string X64Name = Name + "-" + QuantumName + "-x64.dll";
+    public const string X64Name = Name + "-" + QuantumName + "-x64.dll";
 
-    public static string Arm64Name = Name + "-" + QuantumName + "-arm64.dll";
+    public const string Arm64Name = Name + "-" + QuantumName + "-arm64.dll";
 
 #if Q8
-    public static string Quantum = "Q8";
+    public const string Quantum = "Q8";
 #elif Q16
-    public static string Quantum = "Q16";
+    public const string Quantum = "Q16";
 #elif Q16HDRI
-    public static string Quantum = "Q16-HDRI";
+    public const string Quantum = "Q16-HDRI";
 #else
 #error Not implemented!
 #endif
 
 #if OPENMP
-    public static string OpenMP = "-OpenMP";
+    public const string OpenMP = "-OpenMP";
 #else
-    public static string OpenMP = "";
+    public const string OpenMP = "";
 #endif
 }
